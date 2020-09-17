6f6e41cdc58be4d4dddae06020f0da7c3063a0d3
Edan Saedi <Edan.Saedi@GMR.local>
2020-09-16 18:18:44 -0700

    Do the soft-coding.

---
+++ Attribute Practice/Attribute Practice/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using System.IO;
+
+namespace Attribute_Practice
+{
+
+    //Do this practice.
+    //Create an attribute called author. Full name and version they wrote. Using reflection, print out any class that has that attribute applied to it in the assembly.
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = true)]
+    class AuthorAttribute : Attribute
+    {
+        public string Name { get; private set; }
+        public Version Version { get; private set; }
+
+        public AuthorAttribute(string name, int major = 1, int minor = 1)
+        {
+            Name = name;
+            Version = new Version(major, minor);
+        }
+    }
+
+    [Author("Ronel Saedi", 1, 0)]
+    class Animal
+    {
+        public string Species { get; private set; }
+
+        public int Age { get; set; }
+
+        public Animal(string name)
+        {
+            Species = name;
+        }
+
+        public override string ToString()
+        {
+            return Species;
+        }
+    }
+
+    [Author("Ronel Saedi", 1, 0)]
+    [Author("Karan", 1, 1)]
+    class Person
+    {
+        public string Name { get; private set; }
+
+        public int Age { get; set; }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    [Author("Ronel Saedi", 1, 0)]
+    [Author("Karan", 1, 1)]
+    [Author("Grandpa Joe", 2, 2)]
+    class Plant
+    {
+        public string Species { get; private set; }
+
+        public int Age { get; set; }
+
+        public Plant(string name)
+        {
+            Species = name;
+        }
+
+        public override string ToString()
+        {
+            return Species;
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            
+            //Next time, we will go over the non-hard coding method.
+            //var assembly = Assembly.GetExecutingAssembly().GetReferencedAssemblies();
+
+            ;
+
+
+            //var person = new Person("John Brown");
+            //var animal = new Animal("Cheetah");
+            //var plant = new Plant("sunflower");
+
+            //Type personType = person.GetType();
+            //Type animalType = animal.GetType();
+            //Type plantType = plant.GetType();
+
+            //var personCustomAttributes = personType.GetCustomAttributes();
+            //var animalCustomAttributes = animalType.GetCustomAttributes();
+            //var plantCustomAttributes = plantType.GetCustomAttributes();
+
+            //Console.WriteLine("Person");
+            //foreach (var attribute in personCustomAttributes)
+            //{
+            //    if (attribute is AuthorAttribute)
+            //    {
+            //        var author = attribute as AuthorAttribute;
+
+            //        Console.WriteLine($"{author.Name}, {author.Version}");
+            //    }
+            //}
+
+            //Console.WriteLine("Animal");
+            //foreach (var attribute in animalCustomAttributes)
+            //{
+            //    if (attribute is AuthorAttribute)
+            //    {
+            //        var author = attribute as AuthorAttribute;
+
+            //        Console.WriteLine($"{author.Name}, {author.Version}");
+            //    }
+            //}
+
+            //Console.WriteLine("Plant");
+            //foreach (var attribute in plantCustomAttributes)
+            //{
+            //    if (attribute is AuthorAttribute)
+            //    {
+            //        var author = attribute as AuthorAttribute;
+
+            //        Console.WriteLine($"{author.Name}, {author.Version}");
+            //    }
+            //}
+        }
+    }
+}
---
+++ AttributePractice/AttributePractice/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+
+namespace AttributePractice
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = true)]
+    class AuthorAttribute : Attribute
+    {
+        public string Name { get; private set; }
+        public Version Version { get; private set; }
+
+        public AuthorAttribute(string name, int major = 1, int minor = 0)
+        {
+            Name = name;
+            Version = new Version(major, minor);
+        }
+    }
+
+
+    [Author("Karan Bhamra", 1, 1)]
+    [Author("Edan Saedi", 1, 0)]
+    class Person
+    {
+        public string Name { get; private set; }
+
+        public int Age { get; set; }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Method)]
+    class MathOpAttribute : Attribute
+    {
+      
+    }
+
+
+    class CompareByAttribute: Attribute
+    {
+        
+    }
+
+
+    class Calculator
+    {
+        [MathOp]
+        public static int Add(int x, int y) => x + y;
+
+        [MathOp]
+        public static int Mult(int x, int y) => x * y;
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person("John Smith");
+
+            Type personType = person.GetType();
+
+            var customAttributes = personType.GetCustomAttributes();
+
+            foreach (var attribute in customAttributes)
+            {
+                if (attribute is AuthorAttribute)
+                {
+                    var author = attribute as AuthorAttribute;
+
+                    Console.WriteLine($"{author.Name}, {author.Version}");
+                }
+            }
+
+
+            var calcType = typeof(Calculator);
+
+            var methods = calcType.GetMethods();
+
+            var funcs = new List<MethodInfo>();
+
+            foreach (var method in methods)
+            {
+                var methodAttributes = method.GetCustomAttributes();
+
+                foreach (var attribute in methodAttributes)
+                {
+                    if (attribute is MathOpAttribute)
+                    {
+                        funcs.Add(method);
+                    }
+                }
+            }
+
+            Random gen = new Random();
+
+            int num1 = gen.Next(100);
+            int num2 = gen.Next(100);
+
+            foreach (var func in funcs)
+            {
+                Console.WriteLine($"Invoking: {func.Name} with parameters: {num1}, {num2}, Result = {func.Invoke(null, new object[] { num1, num2})}");
+            }
+
+            var bst = new BST3<int>();
+            var bst2 = new BST3<Person>();
+
+        }
+
+
+        class BST<T> where T: IComparable<T>
+        {
+
+        }
+
+
+        class BST2<T>
+        {
+            IComparer<T> comparer;
+            public BST2(IComparer<T> comparer)
+            {
+
+            }
+        }
+
+
+        class School
+        {
+            Person Admin;
+        }
+
+        class BST3<T>
+        {
+            //attribute based
+        }
+
+
+
+    }
+}
