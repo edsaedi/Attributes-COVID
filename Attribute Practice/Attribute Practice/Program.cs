aebb0ce007f22e97efd129936cad8350e9971968
Edan Saedi <Edan.Saedi@GMR.local>
2020-09-23 18:14:24 -0700

    Finish finding the method type.

--- Attribute Practice/Attribute Practice/Program.cs
+++ Attribute Practice/Attribute Practice/Program.cs
@@ -5,6 +5,9 @@ using System.Net;
 using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
 using System.IO;
+using System.Reflection.Metadata.Ecma335;
+using System.Net.WebSockets;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Attribute_Practice
 {
@@ -12,7 +15,7 @@ namespace Attribute_Practice
     //Do this practice.
     //Create an attribute called author. Full name and version they wrote. Using reflection, print out any class that has that attribute applied to it in the assembly.
 
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = true)]
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true)]
     class AuthorAttribute : Attribute
     {
         public string Name { get; private set; }
@@ -82,17 +85,63 @@ namespace Attribute_Practice
         }
     }
 
+
+
     public class Program
     {
+        [Author("Edan Saedi", 1, 0)]
+        public static int HexToDec(string hex)
+        {
+            string hexAlphabet = "0123456789ABCDEF";
+            int finalresult = 0;
+
+            for (int i = 0; i < hex.Length; i++)
+            {
+                int hexValue = hexAlphabet.IndexOf(hex[i]);
+                int columnValue = (int)Math.Pow(16, i);
+                int conversion = hexValue * columnValue;
+                finalresult += conversion;
+
+            }
+            return finalresult;
+        }
+
         public static void Main(string[] args)
         {
+            // write a hex to decimal converter and invoke it using reflection
+            // FF = 255
+            // 2D = 45
+            // A = 10
 
-            
-            //Next time, we will go over the non-hard coding method.
-            //var assembly = Assembly.GetExecutingAssembly().GetReferencedAssemblies();
+            var programType = typeof(Program);//.Assembly;
 
+            var allMethods = programType.GetMethods(BindingFlags.Static | BindingFlags.Public);
+
+            //Find the correct method.
             ;
 
+
+            //string hexInput = Console.ReadLine();
+            //Console.WriteLine(HexToDec(hexInput));
+
+            //var currentAssembly = typeof(Program).Assembly;
+
+            //foreach (var assembly in currentAssembly.DefinedTypes)
+            //{
+            //    foreach (var attribute in assembly.GetCustomAttributes())
+            //    {
+            //        if(attribute is AuthorAttribute)
+            //        {
+            //            var author = attribute as AuthorAttribute;
+
+            //            Console.WriteLine($"{assembly.Name}: {author.Name}, {author.Version}");
+            //        }
+
+            //    }
+            //}
+
+            //Next time, we will go over the non-hard coding method.
+            //var assembly = Assembly.GetExecutingAssembly().GetReferencedAssemblies();
 
             //var person = new Person("John Brown");
             //var animal = new Animal("Cheetah");
